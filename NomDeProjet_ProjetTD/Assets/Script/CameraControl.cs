7a8c35e885bef8f0cf2b67ef65297b2e6ae8fbcc
EzumamaÃ <>
2025-11-25 21:37:15 +0100

    Started Camera controls
    Started the script "Camera Control" Work in Progress

--- NomDeProjet_ProjetTD/Assets/Script/CameraControl.cs
+++ NomDeProjet_ProjetTD/Assets/Script/CameraControl.cs
@@ -1,41 +1,55 @@
-using UnityEngine;
+    using UnityEngine;
 
-public class CameraControl : MonoBehaviour
-{
-
-    [Header ("Camera Settings")]
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _zoomSpeed;
-
-    private Vector3 _cameraPosition;
-
-    private void Start()
+    public class CameraControl : MonoBehaviour
     {
-        _cameraPosition = this.transform.position;
+
+        [Header ("Camera Settings")]
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _zoomSpeed;
+
+        private Vector3 _cameraPosition;
+
+        private void Start()
+        {
+            _cameraPosition = this.transform.position;
+        }
+
+        private void Update()
+        {
+            // Move camera up,down,left,right
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                _cameraPosition.y += _moveSpeed / 10;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                _cameraPosition.y -= _moveSpeed / 10;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                _cameraPosition.x -= _moveSpeed / 10;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                _cameraPosition.x += _moveSpeed / 10;
+            }
+
+            // Zoom forward and backward
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                _cameraPosition.z += _zoomSpeed / 10;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                _cameraPosition.z -= _zoomSpeed / 10;
+            }
+
+            this.transform.position = _cameraPosition;
+        }
     }
-
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.Z))
-        {
-            _cameraPosition.y += _moveSpeed / 10;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            _cameraPosition.y -= _moveSpeed / 10;
-        }
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            _cameraPosition.x -= _moveSpeed / 10;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            _cameraPosition.x += _moveSpeed / 10;
-        }
-
-        this.transform.position = _cameraPosition;
-    }
-}
