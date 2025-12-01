27f5b61a7a250e4e0ccb02698091b0197ddfb557
EzumamaÃ <>
2025-12-01 23:13:55 +0100

    Tower Updrade via UI with Level 3
    It now works, we can upgrade to level 3 the Triple Mel <3 i can die in peace now, i have the same voice as Garou it's not a joke i feel like i'm dying

--- NomDeProjet_ProjetTD/Assets/Script/UI Scripts/TowerUpgradeUI.cs
+++ NomDeProjet_ProjetTD/Assets/Script/UI Scripts/TowerUpgradeUI.cs
@@ -4,13 +4,24 @@ public class TowerUpgradeUI : MonoBehaviour
 {
     private TowerUpgrade _towerUpgradeScript;
 
+    private void Awake()
+    {
+        Debug.Log("TowerUpgradeUI Awakened: " + gameObject.name);
+    }
+
     public void SetUpgrade(TowerUpgrade _upgradeScript)
     {
+        Debug.Log("SetUpgrade CALLED on: " + gameObject.name);
         _towerUpgradeScript = _upgradeScript;
     }
 
-    public void UpgradeClicked()
+    public void UpgradeLvl2Clicked()
     {
         _towerUpgradeScript.UpgradeTowerLevel2();
+    }
+
+    public void UpgradeLvl3Clicked()
+    {
+        _towerUpgradeScript.UpgradeTowerLevel3();
     }
 }
