42c1cd13828ed4b606e7484d7cdb62cda1da71cf
EzumamaÃ <>
2025-12-04 09:54:31 +0100

    UI Txt works
    I guess... the UI for the tower creation is broken now... imma kms

---
+++ NomDeProjet_ProjetTD/Assets/Script/UI Scripts/InGameUI Text.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class InGameUIText : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI _goldCountTxt;
+    [SerializeField] private TextMeshProUGUI _redBlueprintCountTxt;
+    [SerializeField] private TextMeshProUGUI _greenBlueprintCountTxt;
+    [SerializeField] private TextMeshProUGUI _yellowBlueprintCountTxt;
+    //[SerializeField] private TextMeshProUGUI _energyCountTxt;
+
+    void Update()
+    {
+        _goldCountTxt.text = GameManager.Instance.CurrentMoneyAmount.ToString();
+        _redBlueprintCountTxt.text = GameManager.Instance.CurrentRedBlueprintAmount.ToString();
+        _greenBlueprintCountTxt.text = GameManager.Instance.CurrentGreenBlueprintAmount.ToString();
+        _yellowBlueprintCountTxt.text = GameManager.Instance.CurrentYellowBlueprintAmount.ToString();
+        //_energyCountTxt.text = EnergyManager.Instance.CurrentEnergyAmount.ToString();
+    }
+}
