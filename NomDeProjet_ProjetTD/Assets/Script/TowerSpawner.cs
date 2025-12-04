40577811e9d24348aa2a84bdb41a0d0aafa5f137
EzumamaÃ <>
2025-12-04 10:49:32 +0100

    Prepared for Text UI color changed
    The cost of the tower in the ui can now turn red, just need to be assigned <3

--- NomDeProjet_ProjetTD/Assets/Script/TowerSpawner.cs
+++ NomDeProjet_ProjetTD/Assets/Script/TowerSpawner.cs
@@ -1,4 +1,5 @@
-    using UnityEngine;
+using TMPro;
+using UnityEngine;
 
 public class TowerSpawner : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class TowerSpawner : MonoBehaviour
     [SerializeField] private int _gatlingCost;
     [SerializeField] private int _teslaCost;
     [SerializeField] private int _groundCost;
+
+    [Header("Tower Cost UI Text")]
+    [SerializeField] private TextMeshProUGUI _gatlingCostText;
+    [SerializeField] private TextMeshProUGUI _teslaCostText;
+    [SerializeField] private TextMeshProUGUI _groundCostText;
 
     private TowerChoiceUI _choiceUIScript;
     private GameObject _towerChoicePanel;
@@ -34,24 +40,43 @@ public class TowerSpawner : MonoBehaviour
 
     public void GatlingChoice()
     {
-        if(GameManager.Instance.CurrentMoneyAmount > _gatlingCost)
+        if (GameManager.Instance.CurrentMoneyAmount >= _gatlingCost)
         {
             SpawnTower(0);
             GameManager.Instance.LoseMoney(_gatlingCost);
         }
+        //else if (GameManager.Instance.CurrentMoneyAmount < _gatlingCost)
+        //{
+        //    Debug.Log("Not enough money to build Gatling Tower!");
+        //    _gatlingCostText.color = Color.red;
+        //}
 
     }
     //public void TeslaChoice()
     //{
-    //    if(GameManager.Instance.CurrentMoneyAmount > __teslaCost)
+    //    if(GameManager.Instance.CurrentMoneyAmount >= __teslaCost)
+    //    {
     //    SpawnTower(1);
     //    GameManager.Instance.LoseMoney(_teslaCost);
+    //    }
+    //    else if (GameManager.Instance.CurrentMoneyAmount < _teslaCost)
+    //    {
+    //        Debug.Log("Not enough money to build Tesla Tower!");
+    //        _teslaCostText.color = Color.red;
+    //    }
     //}
     //public void GroundChoice()
     //{
-    //    if(GameManager.Instance.CurrentMoneyAmount > _groundCost)
-    //    SpawnTower(2);
-    //    GameManager.Instance.LoseMoney(_groundCost);
+    //    if(GameManager.Instance.CurrentMoneyAmount >= _groundCost)
+    //    {
+    //      SpawnTower(2);
+    //      GameManager.Instance.LoseMoney(_groundCost);
+    //    }
+    //    else if (GameManager.Instance.CurrentMoneyAmount < _groundCost)
+    //    {
+    //        Debug.Log("Not enough money to build Ground Tower!");
+    //        _groundCostText.color = Color.red;
+    //    }
     //}
 
     private void Update()
