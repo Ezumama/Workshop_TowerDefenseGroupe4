c78224b74eae089274fd08b4480ad4f148c71496
EzumamaÃ <>
2025-12-09 14:42:40 +0100

    main menu still
    EUH ALED CA VA ÊTRE HORRIBLE

--- NomDeProjet_ProjetTD/Assets/Script/UI Scripts/MainMenuUI.cs
+++ NomDeProjet_ProjetTD/Assets/Script/UI Scripts/MainMenuUI.cs
@@ -1,10 +1,17 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private GameObject optionPanel;
+    [SerializeField] private Slider _volume;
+    [SerializeField] private AudioSource _gameMusic;
+
+    [SerializeField] private TextMeshProUGUI _textUIFullscreenWindowed;
 
     private void Start()
     {
@@ -32,6 +39,25 @@ public class MainMenuUI : MonoBehaviour
     public void BackButton()
     {
         optionPanel.SetActive(false);
+    }
+
+    public void FullscreenOnOff()
+    {
+        if (Screen.fullScreen == true)
+        {
+            _textUIFullscreenWindowed.SetText("Windowed Mode");
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            _textUIFullscreenWindowed.SetText("Fullscreen Mode");
+            Screen.fullScreen = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _gameMusic.volume = _volume.value;
     }
     #endregion
 }
